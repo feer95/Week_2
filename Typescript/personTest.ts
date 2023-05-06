8346c7f45656aa5b3d4e79028f2b80b106439f14
Feer95 <fernandofs.ffs@gmail.com>
2023-05-06 11:39:48 +0200

    reto

--- Typescript/personTest.ts
+++ Typescript/personTest.ts
@@ -1,7 +1,16 @@
 
 import { Person } from "./person";
+console.log('Inicio de personTest.ts'); // mensaje de depuración
 
 
-let person1 = new Person("Juan", 50, "Calle 125");
+
+const person1 = new Person("Juan", 50, "Calle 125");
 
 person1.printName();
+
+const currentYear = 2023;
+const yearOfBirth = person1.yearOfBirth(currentYear);
+console.log(`Año de nacimiento: ${yearOfBirth}`);
+
+person1.setAddress("Calle 126");
+console.log(`Dirección: ${person1.getAddress()}`);
