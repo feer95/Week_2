b15a70122559020ef77bc8885eecd9b9a54f7489
Feer95 <fernandofs.ffs@gmail.com>
2023-05-06 11:11:03 +0200

    pruebaTs

---
+++ Typescript/person.ts
@@ -0,0 +1,31 @@
+export class Person {
+    // Parámetros
+        // Públicos
+    public name : string;
+    public age : number;
+        //Privados
+    private address : string;
+
+    // Constructor
+    constructor (name:string, age:number, address:string) {
+        this.name = name
+        this.age = age
+        this. address = address
+    }
+
+    // Métodos
+    printName() :void {
+        console.log(this.name);
+    }
+    yearOfBirth(currYear: number) :number {
+        return currYear - this.age;
+    }
+    setAddress(address:string) :void {
+        this.address = address;
+    }
+    getAddress() {
+        return this.address;
+    }
+}
+
+
---
+++ Typescript/personTest.ts
@@ -0,0 +1,7 @@
+
+import { Person } from "./person";
+
+
+let person1 = new Person("Juan", 50, "Calle 125");
+
+person1.printName();
