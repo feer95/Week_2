41d2e0efa9a3877c26da1726dcc33d4a762885e7
Feer95 <fernandofs.ffs@gmail.com>
2023-05-08 10:24:59 +0200

    pruebaVector

--- Typescript/vectorTest.ts
+++ Typescript/vectorTest.ts
@@ -1,1 +1,20 @@
+import { Vector } from "./vector";
+
+let vector1 = new Vector(5, 10);
+vector1.print();
+
+let vector2 = new Vector(5, 10);
+vector2.print();
+
+let suma = vector1.add(vector2);
+suma.print();
+
+let resta = vector1.subs(vector2);
+resta.print();
+
+let multiplicacion = vector1.mult(vector2);
+multiplicacion.print();
+
+let multiplicacionNumero = vector1.multNumber(5);
+multiplicacionNumero.print();
 
